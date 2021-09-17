7c0aa4f20181de3dfb50a9102d66529905494f44
SHARUN VS <sharun@armino.in>
2021-09-17 11:52:34 +0530

    db configs updated

--- util/database.config.js
+++ util/database.config.js
@@ -3,6 +3,7 @@ const { Sequelize } = require("../tools");
 const db = new Sequelize(process.env.DB, process.env.USER, process.env.PASSWORD, {
 	host: process.env.HOST,
 	dialect: process.env.DIALECT,
+	define: { freezeTableName: true },
 });
 
 module.exports = db;
